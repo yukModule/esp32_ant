62168e26eeee7e84e7adce74aa08d5aa571437f0
yukmkm <1797253189@qq.com>
2024-01-28 13:19:58 +0800

    测试

--- pc/main.py
+++ pc/main.py
@@ -4,12 +4,12 @@ from get_bot_ipv4 import get_ip ,scan_ip
 scan_ip()
 print(get_ip())
 
-# # ————————————修改ip————————————
+# # ------------修改ip------------
 # p = tcp_user('192.168.137.109','yuk2')
 # p.thread_listen()
 # p.send_pass()
 
-# # ————————————回车————————————
+# # ------------回车------------
 # while True:
 #     i = str(input())
 #     # 按回车发送
