8b087de9aca8e3015f072e23799fddc89ef346a2
yukmkm <1797253189@qq.com>
2024-03-05 14:43:49 +0800

    位置预测

--- pc/move_control.py
+++ pc/move_control.py
@@ -41,7 +41,7 @@ def goal_now_posture(bot_id):
 def forecast_angle(last_x, last_y, now_x, now_y, now_angle, x, y):
     '''预测角度'''
     global tick, delay
-    v = math.sqrt( (last_x-now_x)**2 +(last_y-now_y**2) ) / tick
+    v = math.sqrt( (last_x-now_x)**2 +(last_y-now_y)**2 ) / tick
     next_x = v*delay*math.cos(math.radians(now_angle)) + now_x # 预测下一时刻坐标
     next_y = v*delay*math.sin(math.radians(now_angle)) + now_y
 
@@ -155,38 +155,3 @@ def line_move(x,y,bot_id,a,r):
     sleep(0.1)
     trim_cmd_and_send(0, 0, bot_id)
     print(bot_id, '已到达', x, y)
-
-# 无预测控制
-# def line_move(x,y,bot_id,a,r): 
-#     '''直线运动到目标点'''
-#     now_x, now_y, now_angle = goal_now_posture(bot_id)
-#     motor_R_l, motor_L_l = 0, 0
-#     r = int(r)
-#     a = int(a)
-#     while allowable_error(now_x, now_y, x, y, r): #如果不在目标范围内 则执行
-#         now_x, now_y, now_angle = goal_now_posture(bot_id) # 通过视觉获取机器人当前坐标
-#         angle_err = now_angle - get_angle(now_x, now_y, x, y) # 获取角度偏差
-#         if angle_err < -180: # 最小偏差角 [-180, 180] angle_err>0 逆时针
-#             angle_err += 360
-#         if angle_err > 180:
-#             angle_err -= 360
-
-#         show_err(angle_err, now_x, now_y) # 在相机上显示
-
-#         # 使机器人朝向始终指向目标点
-#         if angle_err<-1*a : 
-#             motor_L = 0
-#             motor_R = 500
-#         elif angle_err>a :
-#             motor_L = 500
-#             motor_R = 0
-#         else:
-#             motor_R = 500
-#             motor_L = 500
-
-#         # 减轻服务器负担，提高相应速度，只有与上一次数据不同时才发送
-#         motor_L_l, motor_R_l = send_optimize(bot_id, motor_L, motor_R, motor_L_l, motor_R_l)
-
-#     sleep(0.1)
-#     trim_cmd_and_send(0, 0, bot_id)
-#     print(bot_id, '已到达', x, y)
